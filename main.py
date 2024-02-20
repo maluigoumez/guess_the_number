e33773c11fadb1430025aa5a6c1e5c29f66403a3
Malui Gomez <luisagomezrodela@gmail.com>
2024-02-19 23:25:22 -0600

    changes for the user and computer to take turns guessing

--- main.py
+++ main.py
@@ -1,35 +1,46 @@
 import random
 
-# Generate the secret number
-secret_number = random.randint(1, 100)
+# Generate the random number
+random_number = random.randint(1, 100)
 
-# Player's turn
-player_guess = None
-while player_guess != secret_number:
+# Starting guesses for both user and computer
+user_guess = None
+computer_guess = None
+
+
+while user_guess != random_number and computer_guess != random_number:
+
+    #user's turn
+
     try:
-        player_guess = int(input("Guess a number between 1 and 100: "))
+        user_guess = int(input("Guess a number between 1 and 100: "))
     except ValueError:
         print("Invalid input. Please enter an integer.")
-        continue  # Move to the next iteration of the loop
+        continue  
 
-    if player_guess < secret_number:
+    if user_guess < random_number:
         print("Too low! Try again.")
-    elif player_guess > secret_number:
+    elif user_guess > random_number:
         print("Too high! Try again.")
     else:
         print("Congratulations! You guessed the number!")
-        break  # Exit the loop if the player guesses correctly
+        break  # Exit the loop if the user guesses correctly
 
 # Computer's turn (random guess for now)
-computer_guess = random.randint(1, 100)
+    computer_guess = random.randint(1, 100)
+    print(f"The computer guesses: {computer_guess}")
 
-# Check if the computer guessed correctly
-if computer_guess == secret_number:
-    print("The computer guessed the number!")
-else:
-    # You can add more logic here for additional computer turns or further game play
-    print("The computer didn't guess the number.")
+    if computer_guess < random_number:
+        print("Computer's guess is too low.")
+    elif computer_guess > random_number:
+        print("Computer's guess is too high.")
+    else:
+        print("The computer guessed the number!")
+        break  # Exit the loop if the computer guesses correctly
 
+
+
+    
 # Add unit tests (optional, for code quality assurance)
 # import unittest
 # ... (unit test code here)
